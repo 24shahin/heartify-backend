27e31f50d613fc4be69b4fde14063e91d2ae359c
Md. Zahid-Hossain Shahin <126140511+24shahin@users.noreply.github.com>
2025-02-18 12:21:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const { connect } = require("./Database/mongoDBConfig");
 const app = express();
 // middle waer
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://heartify-git-main-24shahins-projects.vercel.app",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 app.use(
   fileUpload({
     useTempFiles: true,
